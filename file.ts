f0caab958c90dbbbd54820a2a131263b32f845ba
JamaliHassan <balochhassan939@gmail.com>
2024-03-27 11:50:24 -0700

    This is  a console based atm apllication

---
+++ file.ts
@@ -0,0 +1,82 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+let accountPin = 2585;
+let accountBalance = 25000; // dollars
+console.log(accountPin);
+
+const pinAnswer = await inquirer.prompt([
+  {
+    name: "atmPin",
+    type: "number",
+    message: "please enter your pin to proceed:",
+  },
+]);
+if (pinAnswer.atmPin === 2585) {
+  const proceed = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      message: "please select one of the transaction mode to proceed the transaction:",
+      choices: ["withdraw", "fast cash", "check balance"],
+    },
+  ]);
+  if (proceed.operation === "withdraw") {
+    let amount = await inquirer.prompt({
+      name: "amount",
+      type: "number",
+      message: "enter the amount you want to withdraw",
+    });
+    if (amount.amount <= accountBalance) {
+      console.log(
+        `withdrawl successful! your remaining balance is: ${
+          accountBalance - amount.amount
+        }`
+      );
+    } else console.log("insufficient balance");
+  } else if (proceed.operation === "fast cash") {
+    let amount = await inquirer.prompt([
+      {
+        name: "fastCash",
+        type: "list",
+        message: "please select one amount",
+        choices: [1000, 2000, 5000, 10000],
+      },
+    ]);
+    if ((amount.fastCash === 1000, 2000, 5000, 10000)) {
+      console.log(
+        `withdrawl successful! your remaining balance is: ${
+          accountBalance - amount.fastCash
+        }`
+      );
+    } else {
+      console.log("insufficient balance");
+    }
+  }
+  if (proceed.operation === "check balance") {
+    console.log(`${accountBalance}`);
+  }
+} else {
+  console.log("incorrect pin");
+}
+//
+
+//     else if(amount <== accountBalance)
+
+// }
+// else if(proceed.operation === "fastCash"){
+//     const amount = await inquirer.prompt([{
+//         name : "fastCash",
+//         type : "list",
+//         message: "select the amount",
+//         choices : [1000, 2000, 5000, 10000]
+//      }])
+// }else if(amount.fastCash === "choices"){
+//     console.log(`your remaining balance is: ${amount.fastCash} -${accountBalance}`)
+// }
+
+// else{
+//     console.log("Incorrect pin")
+// }
